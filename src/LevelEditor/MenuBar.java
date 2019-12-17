7be468d5af8c7f45d557140694404a6382222030
KoderKaran <karanrkhullar@gmail.com>
2019-12-17 17:52:30 -0500

    All hotkeys for menubar

--- src/LevelEditor/MenuBar.java
+++ src/LevelEditor/MenuBar.java
@@ -2,6 +2,7 @@ package LevelEditor;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 
 public class MenuBar extends JMenuBar {
 
@@ -13,14 +14,40 @@ public class MenuBar extends JMenuBar {
 	}
 
 	private void init(){
+		// TODO: MOST OF THE FUNCTIONALITY OF THE BUTTONS ARE PLACEHOLDERS RN!
 		JMenu file = new JMenu("File");
 		JMenu edit = new JMenu("Edit");
 		JMenu run = new JMenu("Run");
-		JMenuItem newProject = new JMenuItem("New Project");
-		JMenuItem open = new JMenuItem("Open");
-		JMenuItem save = new JMenuItem("Save");
-		JMenuItem saveAs = new JMenuItem("Save As");
-		JMenuItem settings = new JMenuItem("Project Settings");
+		JMenuItem newProject = new JMenuItem(new AbstractAction("New Project") {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Its making a new Project!");
+			}
+		});
+		JMenuItem open = new JMenuItem(new AbstractAction("Open") {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Its Opening!");
+			}
+		});
+		JMenuItem save = new JMenuItem(new AbstractAction("Save") {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Its Saving!");
+			}
+		});
+		JMenuItem saveAs = new JMenuItem(new AbstractAction("Save As") {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Its Saving as!");
+			}
+		});
+		JMenuItem settings = new JMenuItem(new AbstractAction("Project Settings") {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("The Project Settings are opening!");
+			}
+		});
+		newProject.setAccelerator(KeyStroke.getKeyStroke('N', Toolkit.getDefaultToolkit().getMenuShortcutKeyMaskEx()));
+		open.setAccelerator(KeyStroke.getKeyStroke('O', Toolkit.getDefaultToolkit().getMenuShortcutKeyMaskEx()));
+		save.setAccelerator(KeyStroke.getKeyStroke('S', Toolkit.getDefaultToolkit().getMenuShortcutKeyMaskEx()));
+		saveAs.setAccelerator(KeyStroke.getKeyStroke('A', Toolkit.getDefaultToolkit().getMenuShortcutKeyMaskEx()));
+		settings.setAccelerator(KeyStroke.getKeyStroke('T', Toolkit.getDefaultToolkit().getMenuShortcutKeyMaskEx()));
 		file.add(newProject);
 		file.add(open);
 		file.add(save);
